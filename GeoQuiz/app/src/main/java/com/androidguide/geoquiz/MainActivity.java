e835c9a11526e6b4d2803b74cc015371637d7a36
lxq <lxq@weetgo.com>
2019-11-28 17:39:30 +0800

    Toast

--- GeoQuiz/app/src/main/java/com/androidguide/geoquiz/MainActivity.java
+++ GeoQuiz/app/src/main/java/com/androidguide/geoquiz/MainActivity.java
@@ -3,12 +3,35 @@ package com.androidguide.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 类成员
+    private Button btnOk;
+    private Button btnCancle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 按钮事件
+        btnOk = findViewById(R.id.btn_ok);
+        btnOk.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.toast_ok, Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnCancle = findViewById(R.id.btn_cancle);
+        btnCancle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.toast_err, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

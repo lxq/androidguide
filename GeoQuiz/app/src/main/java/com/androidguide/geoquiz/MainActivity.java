e602294d8d470cfe9c5841a9ca655543b1d747c2
lxq <lxq@weetgo.com>
2019-11-29 14:03:35 +0800

    增加类，实现类与源和界面的互动。

--- GeoQuiz/app/src/main/java/com/androidguide/geoquiz/MainActivity.java
+++ GeoQuiz/app/src/main/java/com/androidguide/geoquiz/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,20 @@ public class MainActivity extends AppCompatActivity {
     // 类成员
     private Button btnOk;
     private Button btnCancle;
+    private Button btnNext;
+    private TextView tvQuestion;
+
+    // 数组
+    private Question[] mQuestions = new Question[] {
+            new Question(R.string.qu_australia, true),
+            new Question(R.string.qu_ocean, true),
+            new Question(R.string.qu_mideast, false),
+            new Question(R.string.qu_africa, false),
+            new Question(R.string.qu_americas, true),
+            new Question(R.string.qu_asia, true)
+    };
+
+    private  int mCurIdx = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +48,23 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, R.string.toast_err, Toast.LENGTH_SHORT).show();
             }
         });
+
+        tvQuestion = findViewById(R.id.tv_Question);
+        updateQuestion();
+
+        btnNext = findViewById(R.id.btn_Next);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurIdx = (mCurIdx + 1)%mQuestions.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion() {
+        int id = mQuestions[mCurIdx].getTextId();
+        tvQuestion.setText(id);
+        Toast.makeText(MainActivity.this, Integer.toString(mCurIdx), Toast.LENGTH_SHORT).show();
     }
 }

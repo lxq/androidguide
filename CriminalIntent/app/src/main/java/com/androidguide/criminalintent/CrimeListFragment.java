708a150032bb9c35e05833c9f56137891bca8e80
lxq <lxq@weetgo.com>
2019-12-06 16:32:03 +0800

    实现Fragment细节页的修改，即时反应到RecyclerView列表中。

--- CriminalIntent/app/src/main/java/com/androidguide/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/androidguide/criminalintent/CrimeListFragment.java
@@ -33,12 +33,24 @@ public class CrimeListFragment extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        updateUI();
+    }
+
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
 
-        mCrimeAdapter = new CrimeAdapter(crimes);
-        mCrimeRView.setAdapter(mCrimeAdapter);
+        if (null == mCrimeAdapter) {
+            mCrimeAdapter = new CrimeAdapter(crimes);
+            mCrimeRView.setAdapter(mCrimeAdapter);
+        } else {
+            mCrimeAdapter.notifyDataSetChanged();
+        }
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder

0da4d9cb2e9f19965108f039019a19bf405adc35
lxq <lxq@weetgo.com>
2019-12-07 21:48:13 +0800

    实现第11章ViewPager的内容页左右翻动。

---
+++ CriminalIntent/app/src/main/java/com/androidguide/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,65 @@
+package com.androidguide.criminalintent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CrimePagerActivity extends AppCompatActivity {
+    private static final String EXTRA_CRIME_ID = "com.androidguide.criminalintent.crime_id";
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
+    public static Intent newInstance(Context ctx, UUID crimeID) {
+        Intent intent = new Intent(ctx, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeID);
+        return intent;
+    }
+
+    public CrimePagerActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeID = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+
+        mViewPager = findViewById(R.id.crime_view_pager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
+        for (int i = 0; i < mCrimes.size(); i++) {
+            if (mCrimes.get(i).getId().equals(crimeID)) {
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
+    }
+}

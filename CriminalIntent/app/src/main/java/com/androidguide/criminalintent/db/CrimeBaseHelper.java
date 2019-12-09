8ed95c946b64fd4eb75612800364584ff5463262
lxq <lxq@weetgo.com>
2019-12-09 20:28:40 +0800

    增加创建表的SQL语句。

--- CriminalIntent/app/src/main/java/com/androidguide/criminalintent/db/CrimeBaseHelper.java
+++ CriminalIntent/app/src/main/java/com/androidguide/criminalintent/db/CrimeBaseHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.androidguide.criminalintent.db.CrimeSchema.CrimeTable;
+
 public class CrimeBaseHelper extends SQLiteOpenHelper {
     private static final int VERSION = 1;
     private static final String DB_NAME = "crime.db";
@@ -12,11 +14,20 @@ public class CrimeBaseHelper extends SQLiteOpenHelper {
         super(ctx, DB_NAME, null, VERSION);
     }
 
+    // 创建初始数据库
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        sqLiteDatabase.execSQL("create table " + CrimeTable.NAME +
+                "(" +
+                "_id integer primary key autoincrement, " +
+                CrimeTable.Cols.UUID + "," +
+                CrimeTable.Cols.TITLE +"," +
+                CrimeTable.Cols.DATE + "," +
+                CrimeTable.Cols.SOLVED +
+                ")");
     }
 
+    // 根据版本更新数据库
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 

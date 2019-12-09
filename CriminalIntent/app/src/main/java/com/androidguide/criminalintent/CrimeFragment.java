7c87c54eb4a4c817b42a45d00ac5bc6b7c7e928f
lxq <lxq@weetgo.com>
2019-12-09 19:35:53 +0800

    修改7.5.2节中文本内容更新问题。

--- CriminalIntent/app/src/main/java/com/androidguide/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/androidguide/criminalintent/CrimeFragment.java
@@ -72,7 +72,7 @@ public class CrimeFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
+                mCrime.setTitle(charSequence.toString());
             }
 
             @Override
